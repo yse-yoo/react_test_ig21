fd8a6eb5fd6bea91dd988c675de993b95a0730df
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-10 14:13:07 +0900

    updaet

--- src/Information.jsx
+++ src/Information.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function Information() {
+    // TODO: APIサーバからお知らせ情報を取得
+    var informations = [
+        { id: 1, title: '2学期がはじまりました' },
+        { id: 2, title: 'Reactの学習' },
+        { id: 3, title: 'グループ開発について' },
+    ]
     return (
         <div>
             <h2>Information</h2>
+            <ul>
+                {
+                    informations.map(information => (
+                        <li key={information.id}>{information.title}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
