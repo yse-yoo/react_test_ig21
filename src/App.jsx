c3423ba83351bc0a41ddd18d6d7fccb566d9fd0a
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-10 11:50:46 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import './App.css';
 import Message from './Message';
 
 function App() {
+  // userオブジェクト
+  var user = {
+    name: 'Yohei Yoshikawa',
+    age: 48,
+    country: 'Japen',
+  }
   return (
     <div className="App">
       <header className="App-header">
         Hello, React
-        <Message />
+        <Message user={user} />
       </header>
     </div>
   );
--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-function Message() {
+function Message(props) {
+    var user = props.user;
+    console.log(user);
     return (
         <>
             <h2>Message</h2>
             <h3>User Name</h3>
+            <p>Yohei Yoshikawa</p>
             <h3>Age</h3>
+            <p>48</p>
             <h3>Country</h3>
+            <p>Japn</p>
         </>
     )
 }
