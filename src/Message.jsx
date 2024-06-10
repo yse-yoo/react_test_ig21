609df5d1ee5949985c7930cf76dd8323ee97be8d
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-10 11:29:09 +0900

    update

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function Message() {
-  return (
-    <div>Message</div>
-  )
+    return (
+        <>
+            <h2>Message</h2>
+            <h3>User Name</h3>
+            <h3>Age</h3>
+            <h3>Country</h3>
+        </>
+    )
 }
 
 export default Message
