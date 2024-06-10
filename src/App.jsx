8f53494fbe07bef26f538687771443c3f1639b62
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-10 11:21:54 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Hello, React
+        <Message />
       </header>
     </div>
   );
