6a77c31ecd5e77e40e37b7203ba066cb8c606872
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-10 13:27:49 +0900

    update

--- src/CountPage.jsx
+++ src/CountPage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CountPage() {
+  // state変数の定義
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-        <h2>Count Page</h2>
+      <h2>Count Page</h2>
+      <p>{count}</p>
     </div>
   )
 }
