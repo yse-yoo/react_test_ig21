a62078f52c5de4a16e8b77e7b1ebfef0f8604657
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-10 13:35:03 +0900

    update

--- src/CountPage.jsx
+++ src/CountPage.jsx
@@ -4,10 +4,15 @@ function CountPage() {
   // state変数の定義
   const [count, setCount] = useState(0);
 
+  const countUpHandler = () => {
+    console.log('Count up!!!')
+  }
+
   return (
     <div>
       <h2>Count Page</h2>
       <p>{count}</p>
+      <button onClick={countUpHandler}>Count up</button>
     </div>
   )
 }
